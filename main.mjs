d79463fef7c891cd49ef7e68bad5d15de188e435
Alexis033 <10guevarafm1102@gmail.com>
2023-04-04 13:56:23 -0500

    feat: modo dark and cambio foto

--- main.mjs
+++ main.mjs
@@ -1,28 +1,44 @@
-const $navButton= document.querySelector('.nav-button')
-const $navItems = document.querySelector('.nav-links')
-const $navLinks = document.querySelectorAll('.nav__link')
-const $animatedElement = document.querySelector('.section-title');
+const $navButton = document.querySelector(".nav-button");
+const $modeButton = document.querySelector(".button-mode");
+const $navItems = document.querySelector(".nav-links");
+const $navLinks = document.querySelectorAll(".nav__link");
+const $animatedElement = document.querySelector(".section-title");
 
-$navLinks.forEach(li => {
-  li.addEventListener('click', () => {
-    $navItems.classList.toggle('show');
-  })
+$navLinks.forEach((li) => {
+  li.addEventListener("click", () => {
+    $navItems.classList.toggle("show");
+  });
 });
-$navButton.addEventListener('click', ()=>{$navItems.classList.toggle('show')})
+$navButton.addEventListener("click", () => {
+  $navItems.classList.toggle("show");
+});
 
 const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('typewriter-effect')
-      observer.unobserve(entry.target)
+      entry.target.classList.add("typewriter-effect");
+      observer.unobserve(entry.target);
     } else {
-      entry.target.classList.remove('typewriter-effect')
+      entry.target.classList.remove("typewriter-effect");
     }
-  })
-})
+  });
+});
 
-observer.observe($animatedElement)
+observer.observe($animatedElement);
 
-window.addEventListener('unload', () => {
-  observer.disconnect()
-})
+window.addEventListener("unload", () => {
+  observer.disconnect();
+});
+
+$modeButton.addEventListener("click", () => {
+  const body = document.body;
+  const nav = document.querySelector("nav");
+  const aside = document.querySelector(".aside-perfil");
+  const icon = document.querySelector(".icon-m");
+
+  icon.classList.toggle("fa-moon");
+  icon.classList.toggle("fa-sun");
+  body.classList.toggle("dark-mode");
+  nav.classList.toggle("dark-mode-nav");
+  aside.classList.toggle("dark-mode-aside-perfil");
+});
