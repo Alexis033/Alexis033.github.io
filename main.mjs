92b39e82c9d5a0b07eec44d685057a82aed07dc4
Alexis033 <10guevarafm1102@gmail.com>
2023-04-10 12:30:25 -0500

    fix: footer color in darkmode

--- main.mjs
+++ main.mjs
@@ -35,10 +35,12 @@ $modeButton.addEventListener("click", () => {
   const nav = document.querySelector("nav");
   const aside = document.querySelector(".aside-perfil");
   const icon = document.querySelector(".icon-m");
+  const footer = document.querySelector("footer");
 
   icon.classList.toggle("fa-moon");
   icon.classList.toggle("fa-sun");
   body.classList.toggle("dark-mode");
   nav.classList.toggle("dark-mode-nav");
   aside.classList.toggle("dark-mode-aside-perfil");
+  footer.classList.toggle("dark-color");
 });
